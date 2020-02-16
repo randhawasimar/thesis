1e649758a5708c82afe31aae7a0ee7140e88164a
randhawasimar <simar.randhawa@gmail.com>
2020-02-16 01:21:17 +0000

    fix gpus flag

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ python png_to_jpg_converter.py -I ./data/rois/ -O ./data-transforms/rois-jpg/
 python sub_roi_generator.py -I ./data-transforms/rois-jpg/ -O ./data-transforms/rois-sub/ -S ${THESIS_TCNN_SQUARE_SIZE}
 python fold_generator.py -I ./data-transforms/rois-sub/ -N ${THESIS_TCNN_NUM_FOLDS} -S ${THESIS_TCNN_SEED}
 
-docker run -v ./data-transforms/rois-sub/:/workspace/data/ tcnn-trainer
+docker run --gpus all -v $(pwd)/data-transforms/rois-sub/:/workspace/data/ tcnn-trainer
