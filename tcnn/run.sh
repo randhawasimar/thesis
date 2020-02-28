ca85be8516ec414605f275428da9a6b3d1f96378
randhawasimar <simar.randhawa@gmail.com>
2020-02-28 14:57:38 +0000

    script to write results to csv

--- tcnn/run.sh
+++ tcnn/run.sh
@@ -3,4 +3,6 @@ set -e
 
 . ./create_kth.sh
 . ./make_kth_mean.sh
-caffe train -solver ./solver_tcnn3.prototxt -weights ./tcnn3.caffemodel
+caffe train -solver ./solver_tcnn3.prototxt -weights ./tcnn3.caffemodel 2>&1 | tee /workspace/data/trainer.out
+
+cat /workspace/data/trainer.out| tail -n 5|grep accuracy>/workspace/data/accuracy.out
