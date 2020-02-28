5bd0327c6463bc25b0f56c817eed00c4de983201
randhawasimar <simar.randhawa@gmail.com>
2020-02-28 11:16:18 +0000

    eliminate intermediate files

--- tcnn/make_kth_mean.sh
+++ tcnn/make_kth_mean.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 # Compute the mean image from the 4 kth training lmdbs (4 folds)
-# N.B. this is available in data/kth-tips-2b
+# N.B. this is available in EXAMPLE/kth-tips-2b
 
-DATA=/workspace/data/
+EXAMPLE=/workspace/examples/
 TOOLS=/opt/caffe/build/tools/
 
 $TOOLS/compute_image_mean $EXAMPLE/kth_train1_lmdb \
-  $DATA/kth_mean1.binaryproto
+  $EXAMPLE/kth_mean1.binaryproto
 
 $TOOLS/compute_image_mean $EXAMPLE/kth_train2_lmdb \
-  $DATA/kth_mean2.binaryproto
+  $EXAMPLE/kth_mean2.binaryproto
 
 $TOOLS/compute_image_mean $EXAMPLE/kth_train3_lmdb \
-  $DATA/kth_mean3.binaryproto
+  $EXAMPLE/kth_mean3.binaryproto
 
 $TOOLS/compute_image_mean $EXAMPLE/kth_train4_lmdb \
-  $DATA/kth_mean4.binaryproto
+  $EXAMPLE/kth_mean4.binaryproto
 
 # Link one fold
-ln -f -s  kth_mean1.binaryproto $DATA/kth_mean.binaryproto
+ln -f -s  kth_mean1.binaryproto $EXAMPLE/kth_mean.binaryproto
 
 echo "Mean files created, fold 1 linked ."
